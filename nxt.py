2d802106af808a7d76b4ef51f11d13c3581136c8
YarinTheDev <110265000+YarinTheDev@users.noreply.github.com>
2022-12-11 00:44:27 +0200

    Update nxt.py

--- nxt.py
+++ nxt.py
@@ -3,17 +3,27 @@
 from nextcord import Interaction
 from nextcord.ext import commands
 
+# Create a new Discord client
 bot = commands.Bot(command_prefix="!")
+
+# Define an event for when the bot is ready to connect to Discord
 @bot.event
 async def on_ready():
+  # Print a message to the console to confirm that the bot has connected
   print("Im Ready")
 
+# Define an event for when the bot receives a message
 @bot.event
 async def on_message(message):
+  # Ignore messages sent by the bot itself
   if message.author.bot:
     return 
+  #make !github command
   if message.content.startswith("!github"):
+    #respond my github profile link
     await message.channel.send("https://github.com/YarinTheDev")
+    
+    
 # make an slash command named hello
 @bot.slash_command(name="hello")
 async def hello(interaction: Interaction):
